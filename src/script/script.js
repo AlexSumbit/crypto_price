4c45a067c7b6dd7ddc1fdc39d64cad4e377b84d4
AlexSumbit <alexsumbit@gmail.com>
2018-02-04 20:17:27 +0200

    scroll fixes

--- src/script/script.js
+++ src/script/script.js
@@ -19,7 +19,60 @@ window.onload = function(){
                 });
             });
         }
+    };
+
+    let select = {
+        init: function(){
+            let selects = document.querySelectorAll(".js-select");
+            selects.forEach(function(item){
+                let selectItems = item.querySelectorAll(".select-item");
+                let header = item.querySelector(".select__header");
+
+                header.onclick = function(){
+                    item.classList.toggle("select--opened");
+                };
+
+                selectItems.forEach(function(currentItem){
+                    currentItem.onclick = function(){
+                        header.innerHTML = currentItem.innerHTML;
+                        item.classList.remove("select--opened");
+                    };
+                });
+            });
+        }
+    };
+
+    let app = {
+        init: function(){
+            this.getCurrencies();
+        },
+        getCurrencies: function(){
+            let self = this;
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', 'https://api.coindesk.com/v1/bpi/currentprice.json');
+            xhr.onload = function(){
+                if(xhr.readyState == 4 && xhr.status == 200){
+                    let result = xhr.responseText;
+                    self.setCurrencies();
+                }
+            }
+            xhr.send(null);
+        },
+        setCurrencies: function(){
+            let elements = document.querySelectorAll(".js-currencies");
+            elements.forEach(function(element){
+                let tpl = element.querySelector(".js-tpl");
+                for(let i = 0; i < 50; i++){
+                    tpl.content.querySelector("input[name='currency']").value = i;
+                    tpl.content.querySelector("span").innerHTML = i;
+
+                    element.appendChild(document.importNode(tpl.content, true));
+                }
+            });
+            select.init();
+        }
     }
 
+    app.init();
     datePicker.init();
 }
